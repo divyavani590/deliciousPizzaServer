2b7197d78933c47d69e268055461d558f6d45bea
divyavani590 <divyavani590@gmail.com>
2019-03-24 23:24:22 -0400

    New changes

---
+++ README.md
@@ -0,0 +1,10 @@
+//Connecting to server
+
+Open cmd prompy goto .ssh folder under c / users/ divya
+
+type ssh -i "DeliciousPizza.pem" ubuntu@ec2-52-54-227-251.compute-1.amazonaws.com
+
+
+
+on server goto diliciouspizzaserver folder
+type node index.js
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var io = require('socket.io').listen(server);
 var client;
 
 app.get('/', function(req, res) {
-  res.send('Hello');
+  res.send('Hello Divya');
 });
 
 //Whenever someone connects this gets executed
@@ -23,31 +23,48 @@ io.on('connection', function(socket) {
 });
 
 app.get('/dat', function (req, res) {
-  console.log("ssssssssssssss");
+  console.log("Alexa requested Data");
   var response="";
+  console.log(req.query.query2);
   if(req.query.intentName == 'SelectCrustIntent'){
-    response = `Crust number ${req.query.query2} has been selected, Would you like to continue to Meats?`;
+    response = `Crust number ${req.query.query2} has been selected, Would you like to add some Meats?`;
     client.emit('alexaMessage', {intent : req.query.intentName, message: req.query.query2});
   }  
   if(req.query.intentName == 'FlowIntent'){
     console.log(req.query.query2);
     if(req.query.query2 == 1){
-      response = `Please select Meats using numbers next to them?`;
+      response = `Please select Meats using numbers next to them, Please say Meat followed by number, for example Meat 2`;
     }else if(req.query.query2 == 2){
-      response = `Please select Veggies using numbers next to them?`;
+      response = `Please select Veggies using numbers next to them, Please say Veggie followed by number, for example Veggie 2`;
     }else if(req.query.query2 == 3){
-      response = `Please Review your order and confirm if you would like to Place the order?`;
+      response = `Please Review your order and say Place order to Place the order`;
     }else if(req.query.query2 == 4){
       response = `Thank You for your order, your Pizza is geeting ready!`;
     }
     client.emit('alexaMessage', {intent : req.query.intentName, message: req.query.query2});
   }  
   if(req.query.intentName == 'SelectMeatsIntent'){
-    response = `Meats numbered ${req.query.query2} have been selected, Would you like to continue to Veggies?`;
+    response = `Meat number ${req.query.query2} has been selected, If you would like to add more meats say meat number or Say add Veggies to continue to Veggies or Review to Review your order?`;
     client.emit('alexaMessage', {intent : req.query.intentName, message: req.query.query2});
   }
+
+  if(req.query.intentName == 'gotoVeggiesIntent'){
+    response = `Please select Veggies using numbers next to them, Please say Veggie followed by number, for example Veggie 2`;
+    client.emit('alexaMessage', {intent : 'FlowIntent', message: req.query.query2});
+  }
+
+  if(req.query.intentName == 'gotoReviewIntent'){
+    response = `Please Review your order and confirm if you would like to Place the order?`;
+    client.emit('alexaMessage', {intent : 'FlowIntent', message: req.query.query2});
+  }
+
+  if(req.query.intentName == 'gotoPlaceOrderIntent'){
+    response = `Thank You for your order, your Pizza is geeting ready!`;
+    client.emit('alexaMessage', {intent : 'FlowIntent', message: req.query.query2});
+  }
+
   if(req.query.intentName == 'SelectVeggiesIntent'){
-    response = `Veggies numbered ${req.query.query2} have been selected, Would you like to continue to Review your order?`;
+    response = `Veggies numbered ${req.query.query2} have been selected, If you would like to add more Veggies say Veggie number or Say Review to continue to Review your order?`;
     client.emit('alexaMessage', {intent : req.query.intentName, message: req.query.query2});
   }
   
